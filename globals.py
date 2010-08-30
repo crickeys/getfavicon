30bbb24f1eec75ba041b5bc7db267368a739064c
jason <jason@0c1c7bd3-9391-4859-b94e-4e2dcdc6a516>
2010-08-30 11:13:21 +0000

    Cleanup counter lib, increase shards, increase cache time
    git-svn-id: https://potato.codebasehq.com/getfavicon/getfavicon.svn/trunk@25 0c1c7bd3-9391-4859-b94e-4e2dcdc6a516

--- globals.py
+++ globals.py
@@ -5,7 +5,7 @@ war = logging.warning
 err = logging.error
 cri = logging.critical
 
-SHARDS_PER_COUNTER = 100
+SHARDS_PER_COUNTER = 1000
 MC_CACHE_TIME = 2419200 #seconds (28 days)
 DS_CACHE_TIME = 90 #days
 
--- libs/counter/counter.py
+++ libs/counter/counter.py
@@ -6,8 +6,11 @@ from google.appengine.api import memcache
 from globals import *
 from models import *
 
+
 def ChangeCount(nameOfCounter, delta):
+  
   shard_id = '/%s/%s' % (nameOfCounter, random.randint(1, SHARDS_PER_COUNTER))
+  
   def update():
     shard = CounterShard.get_by_key_name(shard_id)
     if shard:
@@ -15,15 +18,23 @@ def ChangeCount(nameOfCounter, delta):
     else:
      shard = CounterShard(key_name=shard_id, name=nameOfCounter, count=delta)
     shard.put()
+    
   db.run_in_transaction(update)
-  
+
+
 def GetCount(nameOfCounter):
+  
   memcache_id = '/CounterShard/%s' %  nameOfCounter
   result = memcache.get(memcache_id)
+  
   if not (result == None):
     return result
+    
   result = 0
+  
   for shard in CounterShard.gql('WHERE name=:1', nameOfCounter):
    result += shard.count
-  memcache.set(memcache_id, result, 60)
+   
+  memcache.set(memcache_id, result, 600)
+  
   return result
